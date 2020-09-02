ab1e9fdb3c5e1af97ae5e213ae10ff135465dca6
tsunoda1 <tsunoda@b2ml.com.br>
2020-09-02 00:35:04 -0300

    feat(styles/dashboard): create styles in dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,61 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Container, Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Container, Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Teste</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input type="text" placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/50559381?s=460&u=777d408b6aa6c3f2125a0661eac89ee5cbffac3f&v=4"
+            alt="Gabriel Tsunoda"
+          />
+          <div>
+            <strong>Projeto Check-in AMBEV</strong>
+            <p>Projeto Criado durante o mega hack 3.0 da Shawee</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/50559381?s=460&u=777d408b6aa6c3f2125a0661eac89ee5cbffac3f&v=4"
+            alt="Gabriel Tsunoda"
+          />
+          <div>
+            <strong>Projeto Check-in AMBEV</strong>
+            <p>Projeto Criado durante o mega hack 3.0 da Shawee</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/50559381?s=460&u=777d408b6aa6c3f2125a0661eac89ee5cbffac3f&v=4"
+            alt="Gabriel Tsunoda"
+          />
+          <div>
+            <strong>Projeto Check-in AMBEV</strong>
+            <p>Projeto Criado durante o mega hack 3.0 da Shawee</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
+import githubBackground from '../assets/github_background.svg';
+
 export default createGlobalStyle`
   *{
     margin: 0;
@@ -9,6 +11,21 @@ export default createGlobalStyle`
   }
 
   body{
-    background: #f0f0f5;
+    background: #f0f0f5 url(${githubBackground}) no-repeat 70% top;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button{
+    font: 16px Roboto, sans-serif;
+  }
+
+  #root{
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 40px 20px;
+  }
+
+  button{
+    cursor: pointer;
   }
 `;
